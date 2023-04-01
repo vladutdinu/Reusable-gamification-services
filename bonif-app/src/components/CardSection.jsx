e39b1dff033913d3958fe06bcb8814e22f685386
vasile.vlasa <vasile.vlasa@student.unitbv.ro>
2023-04-02 01:03:10 +0300

    DB

--- bonif-app/src/components/CardSection.jsx
+++ bonif-app/src/components/CardSection.jsx
@@ -1,13 +1,21 @@
 import './CardSection.scss';
+import coffeSpillUrl from '../assets/coffe_spilling.png';
+import { ReactComponent as BlackCoffee } from '../assets/black-coffee.svg';
 
 const CardSection = () => {
   return (
     <div className="card-section">
-      <div className="title"> Scan the card and collect as many points as possible </div>
-      <div className="suggestin">Don't miss out any offer!</div>
-
+      <section className="before-card">
+        <section className="text-section">
+          <div className="title"> Scan the card and collect as many points as possible </div>
+          <div className="suggestin">Don't miss out any offer!</div>
+        </section>
+        <img src={coffeSpillUrl} alt="Coffee spilling" className="coffee-spilling" />
+      </section>
       <div className="points-card">
-        <div className="top-half"></div>
+        <div className="top-half">
+          <BlackCoffee className="black-coffee" />
+        </div>
         <div className="bottom-half"></div>
       </div>
     </div>
--- bonif-app/src/components/Carousel.jsx
+++ bonif-app/src/components/Carousel.jsx
@@ -1,12 +1,29 @@
 import Quest from './Quest';
 import './Carousel.scss';
+import { useState } from 'react';
 
 const Carousel = ({ questData }) => {
   const quests = ['daily', 'weekly', 'monthly'];
+  const [shift, setShift] = useState(-23);
 
+  const handleClickPoints = (e) => {
+    const parentElement = e.target.parentNode;
+    const childElement = parentElement.querySelector('.active');
+    childElement.classList.remove('active');
+
+    e.target.classList.add('active');
+    if (e.target.classList.contains('option1')) {
+      return setShift(30);
+    } else if (e.target.classList.contains('option2')) {
+      return setShift(-23);
+    } else {
+      return setShift(-75);
+    }
+  };
+  console.log(questData);
   return (
     <div className="carousel">
-      <div className="quests-container">
+      <div className="quests-container" style={{ transform: `translateX(${shift}%)` }}>
         {quests.map((q, i) => {
           return (
             <Quest
@@ -20,9 +37,11 @@ const Carousel = ({ questData }) => {
             />
           );
         })}
-        <div className="point1">.</div>
-        <div className="point2">.</div>
-        <div className="point3">.</div>
+      </div>
+      <div className="dots-container">
+        <div onClick={handleClickPoints} className="option1"></div>
+        <div onClick={handleClickPoints} className="option2"></div>
+        <div onClick={handleClickPoints} className="option3"></div>
       </div>
     </div>
   );
