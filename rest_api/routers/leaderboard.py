5596a03f19f111f333c842f2524eea997bf43256
VladutDinu <dinuionutvladut99@gmail.com>
2023-04-01 21:29:53 +0300

    added leaderboard

--- rest_api/routers/leaderboard.py
+++ rest_api/routers/leaderboard.py
@@ -1,6 +1,7 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, Response
 from utils import leaderboard_crud
+from datetime import date
 from models import model
 from database import SessionLocal
 
@@ -29,11 +30,11 @@ async def get_leaderboard(leaderboard_id: int, db: get_db = Depends()):
     else:
         raise HTTPException(status_code=400, detail="leaderboard doesnt exist")
     
-@router.get("/all/{customer_id}", response_model=List[model.Leaderboard])
-async def get_leaderboards_customer(customer_id: int, db: get_db = Depends()):
-    result = leaderboard_crud.get_leaderboards_customer(customer_id, db)
+@router.get("/all/{current_date}", response_model=model.LeaderboardResult)
+async def get_leaderboards_customer(current_date: date, db: get_db = Depends()):
+    result = leaderboard_crud.get_leaderboard_result(current_date, db)
     if result:
-        return [leaderboard.__dict__ for leaderboard in result]
+        return result.__dict__
     else:
         raise HTTPException(status_code=400, detail="No leaderboards")
 
@@ -45,13 +46,6 @@ async def update_leaderboard(leaderboard: model.Leaderboard, db: get_db = Depend
     else:
         raise HTTPException(status_code=400, detail="leaderboard doesnt exist")
 
-@router.put("/", response_model=model.Leaderboard)
-async def update_leaderboard_status(leaderboard_id: int, db: get_db = Depends()):
-    result = leaderboard_crud.update_leaderboard_status(leaderboard_id, db)
-    if result:
-        return leaderboard_crud.get_leaderboard(leaderboard_id, db).__dict__
-    else:
-        raise HTTPException(status_code=400, detail="leaderboard doesnt exist")
 
 @router.delete("/{leaderboard_id}", response_model=model.Leaderboard)
 async def delete_leaderboard(leaderboard_id: int, db: get_db = Depends()):
