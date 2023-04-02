a6a8acf86f6f92678b37e40e6386ef50324434c9
vasile.vlasa <vasile.vlasa@student.unitbv.ro>
2023-04-02 03:53:33 +0300

    Ready for push

--- bonif-app/src/App.jsx
+++ bonif-app/src/App.jsx
@@ -1,6 +1,5 @@
-import './App.scss';
 import React, { useState } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 // import Quest from './components/Quest';
 // import Header from './components/Header';
 // import Welcome from './components/Welcome';
@@ -12,7 +11,7 @@ import { Register } from './components/Register';
 import './components/LoginRegister.scss';
 import { AuthContext } from './context/AuthContext';
 import { useAuth } from './customHooks/useAuth';
-import { useUser } from './customHooks/useUser';
+import Home from './pages/Home';
 
 function App() {
   const { isAuthenticated } = useAuth();
@@ -23,7 +22,7 @@ function App() {
         <Route path="/" element={<LoginRegister />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<p>home</p>} />
+        <Route path="/home" element={<Home />} />
         <Route path="*" element={<LoginRegister />} />
       </Routes>
     </AuthContext.Provider>
